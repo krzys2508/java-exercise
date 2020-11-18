28e94cdd1e7d4164e891149bafe3e4efcd896ee3
krzys2508 <56433668+krzys2508@users.noreply.github.com>
2020-11-18 21:34:29 +0100

    intermediate stuff

--- src/main/java/training/intermediateoperators/IntermediateOperatorsExercise.java
+++ src/main/java/training/intermediateoperators/IntermediateOperatorsExercise.java
@@ -1,6 +1,9 @@
 package training.intermediateoperators;
 
 import javax.sql.ConnectionPoolDataSource;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -8,22 +11,22 @@ public class IntermediateOperatorsExercise implements StreamIntermediateOperator
 
     @Override
     public Stream<String> wordsLongerThanFiveCharacters(List<String> words) {
-        words.stream().filter(n-> n.getName)
-        return Stream.empty();}
+        return words.stream().filter(n-> n.length()>5);
+        }
 
     @Override
     public Stream<String> firstThreeWordsLongerThanFiveCharacters(List<String> words) {
-        return Stream.empty();
+        return words.stream().filter(n->n.length() > 5).limit(3);
     }
 
     @Override
     public Stream<String> firstThreeDistinctWordsLongerThanFiveCharactersInReverseAlphabeticalOrder(List<String> words) {
-        return Stream.empty();
+       return  words.stream().filter(n->n.length()>5).distinct().limit(3).sorted(Comparator.reverseOrder());
     }
 
     @Override
     public Stream<Integer> lengthOfWords(List<String> words) {
-        return Stream.empty();
+        return words.stream().map(n->n.length());
     }
 
     @Override
