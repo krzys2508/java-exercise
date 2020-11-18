a500ec1e7e0215332516809d7b4baad6c908b7f2
krzys2508 <56433668+krzys2508@users.noreply.github.com>
2020-11-18 20:46:45 +0100

    more streams

--- src/main/java/training/creation/StreamCreationExercise.java
+++ src/main/java/training/creation/StreamCreationExercise.java
@@ -8,28 +8,28 @@ public class StreamCreationExercise implements StreamCreator {
 
     @Override
     public IntStream positiveDigitsUsingOf() {
-        return IntStream.of(1,2,3,4,5,-7,-12,-15).filter(val->val>0);
+        return IntStream.of(-1,0,1,2,3,4,5,6,7,8,9).filter(val->val>0);
     }
 
     @Override
     public IntStream positiveDigitsUsingRange() {
 
-        return IntStream.range(-20,20).filter(val->val>0);
+        return IntStream.range(-20,10).filter(val->val>0);
     }
 
     @Override
     public IntStream positiveDigitsUsingIterate() {
-        return IntStream.iterate(-20,);
+        return IntStream.iterate(-1,n->n+1).limit(11).filter(val->val>0);
     }
 
     @Override
     public IntStream powersOfTwo() {
-        return IntStream.empty();
+        return IntStream.of(1,2,3,4,5,6,7,8,9,10).map(i->(int)Math.pow(2,i));
     }
 
     @Override
     public DoubleStream from0Till1WithTenSteps() {
-        return DoubleStream.empty();
+        return DoubleStream.iterate(0,n->(n+0.1)*100).limit(11);
     }
 
     @Override
